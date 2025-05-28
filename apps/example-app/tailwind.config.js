b5fb0fda195e4d760e190237278c8eff212ee5fa
Ishan <ishantiw.quasar@gmail.com>
2025-05-28 12:28:49 +0200

    chores: setup prettier

--- apps/example-app/tailwind.config.js
+++ apps/example-app/tailwind.config.js
@@ -1,18 +1,18 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-    "../../packages/flow-sdk/src/**/*.{ts,tsx}"
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+    '../../packages/flow-sdk/src/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...fontFamily.sans],
-      },
-    },
+        sans: ['Inter', ...fontFamily.sans]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
