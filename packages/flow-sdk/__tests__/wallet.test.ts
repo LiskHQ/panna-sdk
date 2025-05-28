34bfc81bd0c9d22cda1492b6f595aa20838f4b0a
Ishan <ishantiw.quasar@gmail.com>
2025-05-28 18:02:42 +0200

    chores: improve monorepo setup

---
+++ packages/flow-sdk/__tests__/wallet.test.ts
@@ -0,0 +1,5 @@
+// Adding a simple test to ensure the testing framework is working
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
--- tsup.config.js
+++ tsup.config.js
@@ -7,4 +7,3 @@ export default defineConfig({
   sourcemap: true,
   clean: true
 });
-
