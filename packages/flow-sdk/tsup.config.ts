1787a7eeb6d4cf63e03f70ddd09b378be864b481
Matjaz Verbole <verbole@gmail.com>
2025-05-29 10:48:46 +0200

    Fix general project issues

--- packages/flow-sdk/tsup.config.ts
+++ packages/flow-sdk/tsup.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/ui/index.ts'],
+  entry: {
+    index: 'src/index.ts',
+    core: 'src/core/index.ts',
+    ui: 'src/ui/index.ts'
+  },
   format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
   clean: true,
   splitting: true,
-  external: ['react', 'react-dom'],
-  outDir: 'dist'
+  outDir: 'dist',
+  external: ['react', 'react-dom']
 });
