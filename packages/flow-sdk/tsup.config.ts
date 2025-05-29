a8db802937c2e1ef1e07af76ed89d6b805207180
Matjaz Verbole <verbole@gmail.com>
2025-05-29 11:32:49 +0200

    Expose core functions of Thirdweb client

--- packages/flow-sdk/tsup.config.ts
+++ packages/flow-sdk/tsup.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: {
-    index: 'src/index.ts',
-    core: 'src/core/index.ts',
-    ui: 'src/ui/index.ts'
-  },
+  entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
