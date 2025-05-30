7ee1a017356156228985b9f4b89242713f2b8535
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-05-30 13:35:42 +0100

    (refactor): update and memoize login button

--- apps/example-app/src/components/login-button.tsx
+++ apps/example-app/src/components/login-button.tsx
@@ -1,12 +1,17 @@
+import { useMemo } from 'react';
 import { createThirdwebClient } from 'thirdweb';
 import { ConnectButton, ConnectButtonProps } from 'thirdweb/react';
 
 export type LoginButtonProps = Omit<ConnectButtonProps, 'client'>;
 
-export const LoginButton = (props: LoginButtonProps) => {
-  const client = createThirdwebClient({
-    clientId: process.env.NEXT_PUBLIC_CLIENT_ID || ''
-  });
+export function LoginButton(props: LoginButtonProps) {
+  const client = useMemo(
+    () =>
+      createThirdwebClient({
+        clientId: process.env.NEXT_PUBLIC_CLIENT_ID || ''
+      }),
+    [process.env.NEXT_PUBLIC_CLIENT_ID]
+  );
 
   return (
     <ConnectButton
@@ -15,4 +20,4 @@ export const LoginButton = (props: LoginButtonProps) => {
       {...props}
     />
   );
-};
+}
