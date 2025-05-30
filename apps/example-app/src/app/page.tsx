1681bc3dd5ed79bcb126036e9b220c2d4ffa84aa
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-05-30 20:22:15 +0100

    (refactor): move login button file and minor changes

--- apps/example-app/src/app/page.tsx
+++ apps/example-app/src/app/page.tsx
@@ -1,9 +1,9 @@
-import { LoginButton } from '@/components/login-button';
+import { LoginButton } from 'flow-sdk';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <div className="flex justify-center">
           <span className="font-bold">Lisk Flow POC</span>
         </div>
--- packages/flow-sdk/src/ui/login-button.tsx
+++ packages/flow-sdk/src/ui/login-button.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { createThirdwebClient } from 'thirdweb';
 import { ConnectButton, ConnectButtonProps } from 'thirdweb/react';
 
