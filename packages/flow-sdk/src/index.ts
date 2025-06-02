f21990e3ed1fd896f3f2b51627819d88428a25f7
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-06-02 16:50:59 +0100

    (refactor): update login button to use flow client

--- packages/flow-sdk/src/index.ts
+++ packages/flow-sdk/src/index.ts
@@ -3,3 +3,4 @@ export {
   type CreateFlowClientOptions,
   type FlowClient
 } from './core';
+export * from './ui';
--- packages/flow-sdk/src/ui/login-button.tsx
+++ packages/flow-sdk/src/ui/login-button.tsx
@@ -1,13 +1,13 @@
 import { useMemo } from 'react';
-import { createThirdwebClient } from 'thirdweb';
 import { ConnectButton, ConnectButtonProps } from 'thirdweb/react';
+import { createFlowClient } from '../core';
 
 export type LoginButtonProps = Omit<ConnectButtonProps, 'client'>;
 
 export function LoginButton(props: LoginButtonProps) {
   const client = useMemo(
     () =>
-      createThirdwebClient({
+      createFlowClient({
         clientId: process.env.NEXT_PUBLIC_CLIENT_ID || ''
       }),
     [process.env.NEXT_PUBLIC_CLIENT_ID]
