10c1a3dab1926b6fa5963fe9c054996a2d4fd11b
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-06-02 12:53:24 +0100

    (refactor): minor change

--- packages/flow-sdk/src/ui/login-button.tsx
+++ packages/flow-sdk/src/ui/login-button.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { createThirdwebClient } from 'thirdweb';
 import { ConnectButton, ConnectButtonProps } from 'thirdweb/react';
 
