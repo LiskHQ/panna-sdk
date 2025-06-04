304dc0d687eea2fcd2b3408ff0eadc630f939d99
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-06-04 19:19:52 +0100

    (refactor): add logo information to connect button

--- packages/flow-sdk/src/ui/login-button.tsx
+++ packages/flow-sdk/src/ui/login-button.tsx
@@ -19,6 +19,13 @@ export function LoginButton(props: LoginButtonProps) {
       client={client}
       connectButton={{ label: 'Sign in' }}
       theme={liskTheme}
+      appMetadata={{
+        name: 'Lisk Flow App',
+        logoUrl: 'https://portal-assets.lisk.com/logo/lisk-profile-w.svg'
+      }}
+      connectModal={{
+        titleIcon: 'https://portal-assets.lisk.com/logo/lisk-profile-w.svg'
+      }}
       {...props}
     />
   );
