c68792e5856569e4226f649ba9bde17633a0d717
clemente-xyz <clementeserranosutil@gmail.com>
2025-06-10 12:56:54 +0200

    refactor(ui): Remove commented export of renamed hooks from index.ts

--- packages/flow-sdk/src/ui/index.ts
+++ packages/flow-sdk/src/ui/index.ts
@@ -7,5 +7,4 @@ export { LoginButton, type LoginButtonProps } from './components/login-button';
 
 export { useFlowClient } from './hooks/use-flow-client';
 
-// Export all the renamed hooks
 export * from './hooks';
