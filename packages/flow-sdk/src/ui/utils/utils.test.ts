15b8c983cd2774018be66ee571b71be9bff9687a
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-06-17 20:10:48 +0100

    refactor: replace getDefaultToken with usdc icon link

--- packages/flow-sdk/src/ui/utils/utils.test.ts
+++ packages/flow-sdk/src/ui/utils/utils.test.ts
@@ -1,12 +1,9 @@
 import { Chain } from 'thirdweb';
-import { getDefaultToken } from 'thirdweb/react';
-import { getSupportedTokens } from './utils';
-
-jest.mock('thirdweb/react', () => ({
-  getDefaultToken: jest.fn(() => ({
-    icon: 'mocked-icon-url'
-  }))
-}));
+import {
+  getSupportedTokens,
+  liskSepoliaTokenConfig,
+  liskTokenConfig
+} from './utils';
 
 jest.mock('../../core', () => ({
   lisk: {
@@ -21,9 +18,6 @@ describe('utils', () => {
   describe('getSupportedTokens', () => {
     beforeEach(() => {
       jest.clearAllMocks();
-      (getDefaultToken as jest.Mock).mockReturnValue({
-        icon: 'mocked-icon-url'
-      });
     });
 
     it('should return liskTokenConfig when chain id matches lisk.id', () => {
@@ -35,16 +29,7 @@ describe('utils', () => {
 
       const result = getSupportedTokens(mockChain);
 
-      expect(result).toEqual({
-        '1135': [
-          {
-            address: '0xac485391EB2d7D88253a7F1eF18C37f4242D1A24',
-            name: 'Lisk',
-            symbol: 'LSK',
-            icon: 'ipfs://QmRBakJ259a4RPFkMayjmeG7oL6f1hWqYg4CUDFUSbttNx'
-          }
-        ]
-      });
+      expect(result).toEqual(liskTokenConfig);
     });
 
     it('should return liskSepoliaTokenConfig when chain id does not match lisk.id', () => {
@@ -56,27 +41,13 @@ describe('utils', () => {
 
       const result = getSupportedTokens(mockChain);
 
-      expect(result).toEqual({
-        '4202': [
-          {
-            address: '0x8a21CF9Ba08Ae709D64Cb25AfAA951183EC9FF6D',
-            name: 'Lisk',
-            symbol: 'LSK',
-            icon: 'ipfs://QmRBakJ259a4RPFkMayjmeG7oL6f1hWqYg4CUDFUSbttNx'
-          },
-          {
-            address: '0xed875CABEE46D734F38B5ED453ED1569347c0da8',
-            name: 'USDC',
-            symbol: 'USDC',
-            icon: 'mocked-icon-url'
-          }
-        ]
-      });
+      expect(result).toEqual(liskSepoliaTokenConfig);
     });
 
     it('should return liskSepoliaTokenConfig when chain is undefined', () => {
       const result = getSupportedTokens(undefined);
 
+      expect(result).toEqual(liskSepoliaTokenConfig);
       expect(result['4202']).toHaveLength(2);
     });
   });
