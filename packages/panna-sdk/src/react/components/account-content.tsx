c3a05d0bf4864bb359bbe09c93af88fec31306b6
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-07-16 20:11:49 +0100

    (feat): create otp login ui

--- packages/panna-sdk/src/react/components/account-content.tsx
+++ packages/panna-sdk/src/react/components/account-content.tsx
@@ -1,5 +1,6 @@
 import { useConnectedAccounts } from '@/hooks';
 import { AccountDialogFooter } from './account-dialog-footer';
+import { InputOTPForm } from './input-otp-form';
 import { LoginForm } from './login-form';
 import { DialogContent, DialogHeader, DialogTitle } from './ui/dialog';
 
@@ -16,6 +17,20 @@ export function AccountContent() {
             </DialogTitle>
           </DialogHeader>
           <LoginForm />
+          <AccountDialogFooter />
+        </div>
+      </DialogContent>
+    );
+  }
+
+  if (connectedAccounts.length === 1) {
+    return (
+      <DialogContent>
+        <div className="flex flex-col gap-6">
+          <DialogHeader>
+            <DialogTitle className="text-center">6-digit code</DialogTitle>
+          </DialogHeader>
+          <InputOTPForm />
           <AccountDialogFooter />
         </div>
       </DialogContent>
---
+++ packages/panna-sdk/src/react/components/input-otp-form.tsx
@@ -0,0 +1,106 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from 'input-otp';
+import { LoaderCircleIcon } from 'lucide-react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import z from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from '@/components/ui/form';
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot
+} from '@/components/ui/input-otp';
+import { Button } from './ui/button';
+import { Typography } from './ui/typography';
+
+const formSchema = z.object({
+  code: z
+    .string()
+    .min(6, 'Code must be at least 6 characters.')
+    .max(6, 'Code must be at most 6 characters.')
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+export function InputOTPForm() {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      code: ''
+    }
+  });
+
+  const handleSubmit: SubmitHandler<FormValues> = async (values) => {
+    // Handle form submission
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  };
+
+  const { code } = form.watch();
+  const isInputIncomplete = code.length < 6;
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="flex w-full flex-1 flex-col items-center gap-6"
+      >
+        <div className="text-center">
+          <Typography className="text-neutral-400">
+            Enter the verification code sent to
+          </Typography>
+          <Typography className="mt-0!">cnguyen@gmail.com</Typography>
+        </div>
+        <div className="flex flex-col gap-4">
+          <FormField
+            control={form.control}
+            name="code"
+            render={({ field }) => (
+              <FormItem className="flex w-full flex-col items-center">
+                <FormControl>
+                  <InputOTP
+                    autoFocus
+                    maxLength={6}
+                    pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+                    {...field}
+                    data-testid="login-code-input"
+                    data-type="text"
+                    inputMode="text"
+                  >
+                    <InputOTPGroup className="gap-3">
+                      <InputOTPSlot index={0} />
+                      <InputOTPSlot index={1} />
+                      <InputOTPSlot index={2} />
+                      <InputOTPSlot index={3} />
+                      <InputOTPSlot index={4} />
+                      <InputOTPSlot index={5} />
+                    </InputOTPGroup>
+                  </InputOTP>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="text-center">
+            <Typography variant="small" className="text-neutral-400">
+              Didn't receive the code?{' '}
+            </Typography>
+            <Typography variant="small" color="primary">
+              Resend in 0:45
+            </Typography>
+          </div>
+        </div>
+        <Button type="submit" className="w-full" disabled={isInputIncomplete}>
+          {form.formState.isSubmitting && (
+            <LoaderCircleIcon className="animate-spin text-black" />
+          )}
+          Verify
+        </Button>
+      </form>
+    </Form>
+  );
+}
