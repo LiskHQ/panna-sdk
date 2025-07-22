cafdea28aa61966bece7facd09076b5212951ab9
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-07-22 14:11:30 +0100

    (refactor): update user address retrieval logic

--- packages/panna-sdk/src/react/components/connect-button.tsx
+++ packages/panna-sdk/src/react/components/connect-button.tsx
@@ -2,6 +2,7 @@
 
 import { AccountContent } from './account-content';
 import { AuthProvider, useAuth } from './auth-provider';
+import { USER_ADDRESS } from './input-otp-form';
 import { Button } from './ui/button';
 import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
 
@@ -14,7 +15,10 @@ export function ConnectButton() {
 }
 
 function ConnectButtonInner() {
-  const { userAddress } = useAuth();
+  const { userAddress: contextUserAddress } = useAuth();
+  const isBrowser = typeof window !== 'undefined';
+  const lsUserAddress = isBrowser ? localStorage.getItem(USER_ADDRESS) : null;
+  const userAddress = contextUserAddress || lsUserAddress;
 
   return (
     <Dialog>
