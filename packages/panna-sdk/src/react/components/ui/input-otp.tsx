91b0c99f90430c4f34babbff6a5524e9025b54ec
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-07-23 18:38:32 +0100

    (refactor): minor import update

--- packages/panna-sdk/src/react/components/ui/input-otp.tsx
+++ packages/panna-sdk/src/react/components/ui/input-otp.tsx
@@ -3,7 +3,7 @@
 import { OTPInput, OTPInputContext } from 'input-otp';
 import { MinusIcon } from 'lucide-react';
 import * as React from 'react';
-import { cn } from '@/utils/index';
+import { cn } from '@/utils';
 
 function InputOTP({
   className,
