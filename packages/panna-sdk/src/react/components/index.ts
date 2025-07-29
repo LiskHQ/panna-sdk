d4e2024c356f15d6428bb012ac87d13a503d181b
clemente-xyz <clementeserranosutil@gmail.com>
2025-07-29 11:16:35 +0200

    refactor(index): remove unused exports from components index file

--- packages/panna-sdk/src/react/components/index.ts
+++ packages/panna-sdk/src/react/components/index.ts
@@ -1,4 +1,2 @@
 export { ConnectButton } from './auth/connect-button';
-export { PannaProvider, type PannaProviderProps } from './panna-provider';
-export { LoginButton, type LoginButtonProps } from './auth/login-button';
 export { AccountDialog } from './account/account-dialog';
