e5e6d02bc44fa4f94481228212e39eaff37bf8fd
clemente-xyz <clementeserranosutil@gmail.com>
2025-07-30 11:13:16 +0200

    refactor(button, typography): enhance styling consistency
    - Updated button outline variant to include text-foreground for improved visibility.
    - Initialized typographyVariants with 'text-foreground' to ensure consistent text color across typography components.

--- packages/panna-sdk/src/react/components/ui/typography.tsx
+++ packages/panna-sdk/src/react/components/ui/typography.tsx
@@ -2,7 +2,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 import { cn } from '@/utils';
 
-const typographyVariants = cva('', {
+const typographyVariants = cva('text-foreground', {
   variants: {
     variant: {
       h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
