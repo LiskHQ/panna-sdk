3c1f24b052fdb64e48b3d707bb0cbbbe03dcef7e
clemente-xyz <clementeserranosutil@gmail.com>
2025-08-14 16:10:32 +0200

    feat(components): Add Command, Popover, and DialogStepper components to Panna SDK
    - Introduced Command component for command palette functionality.
    - Added Popover component for enhanced UI interactions.
    - Updated DialogStepper to improve step navigation with context management.
    - Refactored DialogStepper to use useContext for better state handling.
    - Enhanced code organization and styling consistency across new components.

--- packages/panna-sdk/src/react/components/ui/dialog-stepper.tsx
+++ packages/panna-sdk/src/react/components/ui/dialog-stepper.tsx
@@ -3,8 +3,8 @@ import {
   cloneElement,
   createContext,
   ReactElement,
-  use,
   useCallback,
+  useContext,
   useMemo,
   useState
 } from 'react';
@@ -27,17 +27,15 @@ export function DialogStepper({ children }: DialogStepperProps) {
   const [step, setStep] = useState(0);
   const [stepData, setStepData] = useState({});
 
-  const next = (data?: Record<string, unknown>) => {
-    const updatedData = { ...stepData, ...data };
-    setStepData(updatedData);
+  const next = useCallback((data?: Record<string, unknown>) => {
+    setStepData((prevData) => ({ ...prevData, ...(data || {}) }));
     setStep((prev) => prev + 1);
-  };
+  }, []);
 
-  const prev = (data?: Record<string, unknown>) => {
-    const updatedData = { ...stepData, ...data };
-    setStepData(updatedData);
+  const prev = useCallback((data?: Record<string, unknown>) => {
+    setStepData((prevData) => ({ ...prevData, ...(data || {}) }));
     setStep((prev) => prev - 1);
-  };
+  }, []);
 
   const reset = useCallback(() => {
     setStep(0);
@@ -51,18 +49,18 @@ export function DialogStepper({ children }: DialogStepperProps) {
   }, [children]);
 
   return (
-    <>
-      <DialogStepperContext value={{ next, prev, reset, stepData }}>
-        {memoizedChildren[step]}
-      </DialogStepperContext>
-    </>
+    <DialogStepperContext.Provider value={{ next, prev, reset, stepData }}>
+      {memoizedChildren[step]}
+    </DialogStepperContext.Provider>
   );
 }
 
-const DialogStepperContext = createContext({} as DialogStepperContextValue);
+const DialogStepperContext = createContext<
+  DialogStepperContextValue | undefined
+>(undefined);
 
 export function useDialogStepper() {
-  const context = use(DialogStepperContext);
+  const context = useContext(DialogStepperContext);
   if (!context) {
     throw new Error(
       'useDialogStepperContext must be used within a DialogStepperProvider'
