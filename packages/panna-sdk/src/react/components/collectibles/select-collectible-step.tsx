3123d2f84cba681aa9d119f9aa2970c40f92c0fc
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-09-25 22:05:22 +0100

    refactor: create collectible form schema and integrate into flow

--- packages/panna-sdk/src/react/components/collectibles/select-collectible-step.tsx
+++ packages/panna-sdk/src/react/components/collectibles/select-collectible-step.tsx
@@ -1,20 +1,48 @@
-import { Token, TokenInstance } from 'src/core';
+import {
+  DialogClose,
+  DialogDescription,
+  DialogTitle
+} from '@radix-ui/react-dialog';
+import { ArrowLeftIcon, XIcon } from 'lucide-react';
+import { UseFormReturn } from 'react-hook-form';
 import { Button } from '../ui/button';
 import { Card, CardContent } from '../ui/card';
+import { DialogHeader } from '../ui/dialog';
+import { useDialogStepper } from '../ui/dialog-stepper';
 import { Typography } from '../ui/typography';
 import { ImageRenderer } from './collectibles-list';
+import { SendCollectibleFormData } from './schema';
 
 type SelectCollectibleStepProps = {
-  collectible: TokenInstance;
-  token: Token;
+  form: UseFormReturn<SendCollectibleFormData>;
 };
 
-export function SelectCollectibleStep({
-  collectible,
-  token
-}: SelectCollectibleStepProps) {
+export function SelectCollectibleStep({ form }: SelectCollectibleStepProps) {
+  const { collectible, token } = form.getValues();
+  const { next } = useDialogStepper();
+
   return (
     <div className="flex flex-col gap-6">
+      <DialogDescription className="sr-only">
+        Send {token.name}
+      </DialogDescription>
+      <DialogHeader className="items-center gap-0">
+        <DialogTitle className="sr-only">Send {token.name}</DialogTitle>
+        <div className="flex w-full items-center justify-between gap-2">
+          <button type="button">
+            <ArrowLeftIcon
+              size={20}
+              className="text-muted-foreground hover:text-primary transition-colors"
+            />
+          </button>
+          <DialogClose>
+            <XIcon
+              size={20}
+              className="text-muted-foreground hover:text-primary transition-colors"
+            />
+          </DialogClose>
+        </div>
+      </DialogHeader>
       <Card className="p-0">
         <CardContent className="p-0">
           <ImageRenderer instance={collectible} />
@@ -23,7 +51,7 @@ export function SelectCollectibleStep({
       <Typography variant="large">
         {token.name} #{collectible.id}
       </Typography>
-      <Button>Send</Button>
+      <Button onClick={() => next()}>Send</Button>
     </div>
   );
 }
--- packages/panna-sdk/src/react/components/collectibles/send-collectible-form.tsx
+++ packages/panna-sdk/src/react/components/collectibles/send-collectible-form.tsx
@@ -1,14 +1,9 @@
-import { ArrowLeftIcon, XIcon } from 'lucide-react';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle
-} from '../ui/dialog';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { DialogStepper } from '../ui/dialog-stepper';
+import { Form } from '../ui/form';
 import { useCollectiblesInfo } from './collectibles-provider';
+import { SendCollectibleFormData, sendCollectibleFormSchema } from './schema';
 import { SelectCollectibleStep } from './select-collectible-step';
 
 type SendCollectibleFormProps = {
@@ -17,44 +12,27 @@ type SendCollectibleFormProps = {
 
 export function SendCollectibleForm({ onClose }: SendCollectibleFormProps) {
   const { activeCollectible, activeToken } = useCollectiblesInfo();
+  const form = useForm<SendCollectibleFormData>({
+    resolver: zodResolver(sendCollectibleFormSchema),
+    defaultValues: {
+      collectible: activeCollectible,
+      token: activeToken,
+      recipientAddress: ''
+    }
+  });
 
   if (!activeCollectible || !activeToken) {
     return null;
   }
 
   return (
-    <Dialog open onOpenChange={onClose}>
-      <DialogContent showCloseButton={false} className="flex flex-col gap-6">
-        <DialogDescription className="sr-only">
-          Send {activeToken.name}
-        </DialogDescription>
-        <DialogHeader className="items-center gap-0">
-          <DialogTitle className="sr-only">Send {activeToken.name}</DialogTitle>
-          <div className="flex w-full items-center justify-between gap-2">
-            <button type="button">
-              <ArrowLeftIcon
-                size={20}
-                className="text-muted-foreground hover:text-primary transition-colors"
-              />
-            </button>
-            <DialogClose>
-              <XIcon
-                size={20}
-                className="text-muted-foreground hover:text-primary transition-colors"
-              />
-            </DialogClose>
-          </div>
-        </DialogHeader>
-        <form>
-          <DialogStepper>
-            <SelectCollectibleStep
-              collectible={activeCollectible}
-              token={activeToken}
-            />
-            <div />
-          </DialogStepper>
-        </form>
-      </DialogContent>
-    </Dialog>
+    <Form {...form}>
+      <form>
+        <DialogStepper>
+          <SelectCollectibleStep form={form} />
+          <div />
+        </DialogStepper>
+      </form>
+    </Form>
   );
 }
