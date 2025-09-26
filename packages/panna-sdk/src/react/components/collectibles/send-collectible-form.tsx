96677a98828a69d8c2e5e002d29cdce94015c0d0
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-09-26 19:32:52 +0100

    refactor: add remaining send collectible steps and update flow

--- packages/panna-sdk/src/react/components/collectibles/send-collectible-form.tsx
+++ packages/panna-sdk/src/react/components/collectibles/send-collectible-form.tsx
@@ -1,10 +1,14 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { SendSuccessStep } from '../send/send-success-step';
 import { DialogStepper } from '../ui/dialog-stepper';
 import { Form } from '../ui/form';
 import { useCollectiblesInfo } from './collectibles-provider';
+import { ProcessingStep } from './processing-step';
 import { SendCollectibleFormData, sendCollectibleFormSchema } from './schema';
 import { SelectCollectibleStep } from './select-collectible-step';
+import { SelectRecipientStep } from './select-recipient-step';
+import { SummaryStep } from './summary-step';
 
 type SendCollectibleFormProps = {
   onClose: () => void;
@@ -30,7 +34,10 @@ export function SendCollectibleForm({ onClose }: SendCollectibleFormProps) {
       <form>
         <DialogStepper>
           <SelectCollectibleStep form={form} />
-          <div />
+          <SelectRecipientStep form={form} />
+          <SummaryStep form={form} />
+          <ProcessingStep form={form} />
+          <SendSuccessStep onClose={onClose} />
         </DialogStepper>
       </form>
     </Form>
---
+++ packages/panna-sdk/src/react/components/collectibles/summary-step.tsx
@@ -0,0 +1,42 @@
+import { UseFormReturn } from 'react-hook-form';
+import { Button } from '../ui/button';
+import { Card, CardContent } from '../ui/card';
+import { DialogHeader, DialogTitle } from '../ui/dialog';
+import { useDialogStepper } from '../ui/dialog-stepper';
+import { Typography } from '../ui/typography';
+import { ImageRenderer } from './collectibles-list';
+import { SendCollectibleFormData } from './schema';
+
+type SummaryStepProps = {
+  form: UseFormReturn<SendCollectibleFormData>;
+};
+
+export function SummaryStep({ form }: SummaryStepProps) {
+  const { collectible, token, recipientAddress } = form.getValues();
+  const { next } = useDialogStepper();
+
+  return (
+    <div className="flex flex-col gap-6">
+      <DialogHeader className="items-center gap-0">
+        <DialogTitle>Summary</DialogTitle>
+      </DialogHeader>
+      <div className="flex flex-col items-center gap-3">
+        <Card className="h-50 w-50 p-0">
+          <CardContent className="p-0">
+            <ImageRenderer instance={collectible} />
+          </CardContent>
+        </Card>
+        <Typography className="mt-0!">
+          {token.name} #{collectible.id}
+        </Typography>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Typography>Send to</Typography>
+        <Typography variant="small" className="text-primary mt-0!">
+          {recipientAddress}
+        </Typography>
+      </div>
+      <Button onClick={() => next()}>Send</Button>
+    </div>
+  );
+}
