898ae4420d528da176078c83d32fa4b49a08fa9e
Matjaz Verbole <verbole@gmail.com>
2025-10-07 08:14:28 +0200

    revert react changes

--- packages/panna-sdk/src/react/components/activity/activity-item.tsx
+++ packages/panna-sdk/src/react/components/activity/activity-item.tsx
@@ -200,20 +200,6 @@ function renderActivityNameOrSymbol(activity: Activity) {
   }
 }
 
-function renderActivityFiatValue(activity: Activity) {
-  const amount = activity.amount as EtherAmount | ERC20Amount | ERC1155Amount;
-
-  if (!amount.fiatValue) {
-    return <Typography variant="muted">-</Typography>;
-  }
-
-  return (
-    <Typography variant="muted">
-      ${amount.fiatValue.amount.toFixed(2)}
-    </Typography>
-  );
-}
-
 export function ActivityItem({ activity }: ActivityItemProps) {
   return (
     <section className="flex w-full justify-between">
@@ -225,10 +211,11 @@ export function ActivityItem({ activity }: ActivityItemProps) {
         </div>
       </header>
       <div className="flex flex-col justify-center text-right">
+        {/* @TODO: Implement pricing when API is updated */}
         <Typography variant="small">
           {renderActivityNameOrSymbol(activity)}
         </Typography>
-        {renderActivityFiatValue(activity)}
+        <Typography variant="muted">$0</Typography>
       </div>
     </section>
   );
