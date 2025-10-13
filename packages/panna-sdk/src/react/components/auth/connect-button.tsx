5b7adb79f2d59b28c3145e28eae40232dc3701e1
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-10-13 16:25:50 +0100

    refactor: update connect button to use props

--- packages/panna-sdk/src/react/components/auth/connect-button.tsx
+++ packages/panna-sdk/src/react/components/auth/connect-button.tsx
@@ -12,13 +12,52 @@ import { LoginButton } from './login-button';
  * @throws {Error} When used outside of PannaProvider context or when no client is available
  *
  * @example
+ *
+ * ## Default Usage
  * ```tsx
  * <PannaProvider clientId="your-client-id" partnerId="your-partner-id">
  *   <ConnectButton />
  * </PannaProvider>
  * ```
+ *
+ * ### Customizing the connect button UI
+ *
+ * ```tsx
+ * <ConnectButton
+ *    connectButton={{
+ *      title: "Sign in to MyApp",
+ *    }}
+ * />
+ * ```
+ *
+ * ### Customizing the dialog UI
+ *
+ * ```tsx
+ * <ConnectButton
+ *    connectDialog={{
+ *      title: "Sign in to MyApp",
+ *      description: "Simple and secure login for myApp",
+ *      otpTitle: "Enter your OTP",
+ *      otpDescription: "Please enter the OTP sent to your email."
+ *    }}
+ * />
  */
-export function ConnectButton() {
+export type ConnectButtonProps = {
+  connectButton?: {
+    title?: string;
+  };
+  connectDialog?: {
+    title?: string;
+    description?: string;
+    otpTitle?: string;
+    otpDescription?: string;
+  };
+};
+
+export function ConnectButton({
+  connectButton,
+  connectDialog
+}: ConnectButtonProps) {
   const account = useActiveAccount();
 
   return (
@@ -28,7 +67,10 @@ export function ConnectButton() {
           <AccountDialog address={account.address} />
         </AccountViewProvider>
       ) : (
-        <LoginButton />
+        <LoginButton
+          connectButton={connectButton}
+          connectDialog={connectDialog}
+        />
       )}
     </>
   );
--- packages/panna-sdk/src/react/components/auth/login-button.tsx
+++ packages/panna-sdk/src/react/components/auth/login-button.tsx
@@ -4,15 +4,19 @@ import { useDialog } from '@/hooks/use-dialog';
 import { Button } from '../ui/button';
 import { Dialog, DialogTrigger } from '../ui/dialog';
 import { AuthFlow } from './auth-flow';
+import { ConnectButtonProps } from './connect-button';
 
-export function LoginButton() {
+export function LoginButton({
+  connectButton,
+  connectDialog
+}: ConnectButtonProps) {
   const { isOpen, setIsOpen } = useDialog();
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
-        <Button>Connect Wallet</Button>
+        <Button>{connectButton?.title || 'Connect Wallet'}</Button>
       </DialogTrigger>
-      <AuthFlow />
+      <AuthFlow connectDialog={connectDialog} />
     </Dialog>
   );
 }
