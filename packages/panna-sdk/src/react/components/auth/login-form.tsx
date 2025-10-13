b07dac6d947980c5f2eba540db82a8da6baba8d9
Ikemefuna Obioha <ikemoobioha@gmail.com>
2025-10-13 21:41:03 +0100

    refactor: extracted schema to separate file

--- packages/panna-sdk/src/react/components/auth/login-form.tsx
+++ packages/panna-sdk/src/react/components/auth/login-form.tsx
@@ -1,10 +1,6 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import {
-  isValidPhoneNumber,
-  parsePhoneNumberWithError
-} from 'libphonenumber-js';
 import { MailIcon, MoveRightIcon, PhoneIcon } from 'lucide-react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -28,45 +24,13 @@ import { Input } from '@/components/ui/input';
 import { usePanna } from '@/hooks/use-panna';
 import { GoogleIcon } from '../icons/google';
 import { DialogStepperContextValue } from '../ui/dialog-stepper';
+import { formSchema } from './schema';
 
 type LoginFormProps = {
   next: DialogStepperContextValue['next'];
   goToStep: DialogStepperContextValue['goToStep'];
 };
-
-const notBlank = (val?: string) => !!val && val.trim() !== '';
-// Move schema to new file
 // Add country code flag selector to phone input
-const formSchema = z
-  .object({
-    email: z
-      .string()
-      .min(7, { message: 'Email should be filled.' })
-      .email('This is not a valid email.')
-      .optional()
-      .or(z.literal('')),
-    phoneNumber: z
-      .string()
-      .min(10, { message: 'Phone number must be at least 10 digits' })
-      .refine(isValidPhoneNumber, {
-        message: 'Please specify a valid phone number'
-      })
-      .transform((value) => parsePhoneNumberWithError(value).number.toString())
-      .optional()
-      .or(z.literal(''))
-  })
-  .refine(
-    (data) => {
-      const emailFilled = notBlank(data.email);
-      const phoneFilled = notBlank(data.phoneNumber);
-      // Only ONE of the fields must be filled
-      return (emailFilled && !phoneFilled) || (!emailFilled && phoneFilled);
-    },
-    {
-      message: 'You must provide either an email OR a phone number—never both.',
-      path: ['email', 'phoneNumber']
-    }
-  );
 
 const GOOGLE_LOGIN_STEP = 2;
 
