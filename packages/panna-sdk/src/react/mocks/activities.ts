68a16405a5ba4cfa5f2d1afeb5f6354193cd5a02
clemente-xyz <clementeserranosutil@gmail.com>
2025-10-15 13:11:04 +0200

    tests(activity): Update activity fixtures and tests to include timestamps for improved tracking
    - Added timestamps to various activity entries in activity.fixture.test.ts for better chronological tracking.
    - Updated ActivityItem and useActivities tests to include timestamps, ensuring consistency across components.
    - Enhanced mock activities in activities.ts with timestamps to reflect realistic scenarios in tests.

--- packages/panna-sdk/src/react/mocks/activities.ts
+++ packages/panna-sdk/src/react/mocks/activities.ts
@@ -33,7 +33,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           }
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-10T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.RECEIVED,
@@ -61,7 +62,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           }
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-09T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.SENT,
@@ -79,7 +81,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           icon: ethIcon
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-08T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.MINTED,
@@ -105,7 +108,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           }
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-07T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.SENT,
@@ -131,7 +135,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           }
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-06T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.RECEIVED,
@@ -158,7 +163,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           }
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-05T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.SENT,
@@ -176,7 +182,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNTAwIiBoZWlnaHQ9IjI1MDAiIHZpZXdCb3g9IjAgMCAzMiAzMiI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48Y2lyY2xlIGN4PSIxNiIgY3k9IjE2IiByPSIxNiIgZmlsbD0iIzYyN0VFQSIvPjxnIGZpbGw9IiNGRkYiIGZpbGwtcnVsZT0ibm9uemVybyI+PHBhdGggZmlsbC1vcGFjaXR5PSIuNjAyIiBkPSJNMTYuNDk4IDR2OC44N2w3LjQ5NyAzLjM1eiIvPjxwYXRoIGQ9Ik0xNi40OTggNEw5IDE2LjIybDcuNDk4LTMuMzV6Ii8+PHBhdGggZmlsbC1vcGFjaXR5PSIuNjAyIiBkPSJNMTYuNDk4IDIxLjk2OHY2LjAyN0wyNCAxNy42MTZ6Ii8+PHBhdGggZD0iTTE2LjQ5OCAyNy45OTV2LTYuMDI4TDkgMTcuNjE2eiIvPjxwYXRoIGZpbGwtb3BhY2l0eT0iLjIiIGQ9Ik0xNi40OTggMjAuNTczbDcuNDk3LTQuMzUzLTcuNDk3LTMuMzQ4eiIvPjxwYXRoIGZpbGwtb3BhY2l0eT0iLjYwMiIgZD0iTTkgMTYuMjJsNy40OTggNC4zNTN2LTcuNzAxeiIvPjwvZz48L2c+PC9zdmc+'
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-04T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.MINTED,
@@ -203,7 +210,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           }
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-03T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.SENT,
@@ -231,7 +239,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           }
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-02T10:00:00.000000Z'
     },
     {
       activityType: TransactionActivity.MINTED,
@@ -258,7 +267,8 @@ export const mockActivities: GetActivitiesByAddressResult = {
           }
         }
       },
-      status: 'success'
+      status: 'success',
+      timestamp: '2024-01-01T10:00:00.000000Z'
     }
   ],
   metadata: {
